b399ed4cde12fdcee12e9bd3d0fb35c20e5b45c5
anlu <anlu@vipkid.com.cn>
2020-03-23 21:43:38 +0800

    配置调整

--- src/main/java/com/fulltext/project/constants/ConstantValue.java
+++ src/main/java/com/fulltext/project/constants/ConstantValue.java
@@ -12,11 +12,11 @@ public class ConstantValue {
     public static final String USER_SESSION_KEY = "USER_SESSION";
     public static final String USER_ROLE_SESSION_KEY = "USER_ROLE_SESSION";
 
-//    public static final String TASK_ATTACHMENT_FILE_PATH = "/Users/anlu/Documents/个人/FullTextProject/task/attachment/";
-//    public static final String NOTICE_ATTACHMENT_FILE_PATH = "/Users/anlu/Documents/个人/FullTextProject/notice/attachment/";
-//    public static final String DOCUMENT_PREVIEW_FILE_PATH = "/Users/anlu/Documents/个人/FullTextProject/preview/";
+    public static final String TASK_ATTACHMENT_FILE_PATH = "/Users/anlu/Documents/个人/FullTextProject/task/attachment/";
+    public static final String NOTICE_ATTACHMENT_FILE_PATH = "/Users/anlu/Documents/个人/FullTextProject/notice/attachment/";
+    public static final String DOCUMENT_PREVIEW_FILE_PATH = "/Users/anlu/Documents/个人/FullTextProject/preview/";
 
-    public static final String TASK_ATTACHMENT_FILE_PATH = "D:/task_attachment";
-    public static final String NOTICE_ATTACHMENT_FILE_PATH = "D:/notice_attachment";
-    public static final String DOCUMENT_PREVIEW_FILE_PATH = "D:/document_preview";
+//    public static final String TASK_ATTACHMENT_FILE_PATH = "D:/task_attachment";
+//    public static final String NOTICE_ATTACHMENT_FILE_PATH = "D:/notice_attachment";
+//    public static final String DOCUMENT_PREVIEW_FILE_PATH = "D:/document_preview";
 }
